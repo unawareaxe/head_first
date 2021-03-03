603e334e4713e96a5ab8f13206ec1edeccaa4960
Ryland Dean <ryland.dean@initlive.com>
2021-03-02 20:44:45 -0500

    Added comments

--- multichoice/questions.js
+++ multichoice/questions.js
@@ -68,9 +68,11 @@ var questions = [
 ];
 
 function listQuestions() {
+    // LOOPING THROUGH QUESTIONS
     for (var index in questions) {
+        // MAKING THE QUESTION HTML ELEMENT
         $('#question-container').append('<div id="question'+ index +'" class="question"><h3>'+ questions[index].question +'</h3></div>');
-
+        // MAKING THE ANSWER BUTTON HTML AND PUTTING IT IN OUR QUESTION HTML ELEMENT
         for (var answerIndex in questions[index].answers) {
             $('#question'+index).append('<div class="answer-buttons"><button id="question'+ index +'answer'+ answerIndex +'" class="answer-button" onclick="checkAnswer('+ questions[index].answers[answerIndex].is_correct +',event)">'+ questions[index].answers[answerIndex].answer +'</button></div>')
         }
@@ -78,13 +80,13 @@ function listQuestions() {
 }
 
 function checkAnswer(isCorrect,event) {
-    console.log(event.target.id);
-
+    // LOOPING ALL ANSWER BUTTONS AND REMOVING CLASSES
     $('.answer-button').each(function() {
         $(this).removeClass('btn-success');
         $(this).removeClass('btn-danger');
     })
 
+    // CHECKING IF THE CLICKED ANSWER IS CORRECT AND ADDING CLASS FOR STYLING
     if (isCorrect == true) {
         $('#'+event.target.id).addClass('btn-success')
     } else {
